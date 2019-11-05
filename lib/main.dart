ccf211935408500c3a92aa01e95955e11c234109
s00083446 <s00083446@mail.itsligo.ie>
2019-11-05 03:18:32 +0000

    Expanded Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(MaterialApp(
 // Custom widget
 class Home extends StatelessWidget {
   // State CAN'T change overtime
-  @override  // Use following build function, instead of inherited 'StatelessWidget'
+  @override // Use following build function, instead of inherited 'StatelessWidget'
   Widget build(BuildContext context) {
     // When we make changes, 'build' function will reload when saved
     return Scaffold(
@@ -16,34 +16,66 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Row(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('hello'),
-              Text('world')
-            ],
+          Expanded(child: Image.asset('assets/galaxy.jpg'),
+          flex: 3 ,
           ),
-        Container(
-          padding: EdgeInsets.all(20.0),
-          color: Colors.green,
-          child: Text('one'),
-        ),
-         Container(
-          padding: EdgeInsets.all(30.0),
-          color: Colors.white,
-          child: Text('two')
-        ),
-         Container(
-          padding: EdgeInsets.all(40.0),
-          color: Colors.orange,
-          child: Text('three')
-        )
-        ] ,)
-      //  Row(  
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+          
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.yellow,
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.blue,
+              child: Text('3'),
+            ),
+          )
+        ],
+      )
+      // Column(
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: <Widget>[
+      //     Row(
+      //       children: <Widget>[
+      //         Text('hello'),
+      //         Text('world')
+      //       ],
+      //     ),
+      //   Container(
+      //     padding: EdgeInsets.all(20.0),
+      //     color: Colors.green,
+      //     child: Text('one'),
+      //   ),
+      //    Container(
+      //     padding: EdgeInsets.all(30.0),
+      //     color: Colors.white,
+      //     child: Text('two')
+      //   ),
+      //    Container(
+      //     padding: EdgeInsets.all(40.0),
+      //     color: Colors.orange,
+      //     child: Text('three')
+      //   )
+      //   ] ,)
+      //  Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       //   crossAxisAlignment: CrossAxisAlignment.start, // best to use Container and do changes to margin and padding inside
       //   children: <Widget>[
       //     Text('Hello world'),
@@ -75,22 +107,22 @@ class Home extends StatelessWidget {
       //     },
       //     icon: Icon( Icons.alternate_email),
       //     color: Colors.amber,
-          //   Icons.mail
-          // ),
-          // label: Text('mail me'),
-          // color: Colors.amber,
-          // child: Text('click me'),
-          // color: Colors.lightBlue
-          // Icons.airport_shuttle,
-          // color: Colors.lightBlue,
-          // size: 50.0
-        //)
-          // child: Image.asset('assets/galaxy2.jpg'),
-          // child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/LH_95.jpg/330px-LH_95.jpg'),
-          //image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/LH_95.jpg/330px-LH_95.jpg'),
-          // image: AssetImage('assets/galaxy.jpg'),
-     //   )
-     ,
+      //   Icons.mail
+      // ),
+      // label: Text('mail me'),
+      // color: Colors.amber,
+      // child: Text('click me'),
+      // color: Colors.lightBlue
+      // Icons.airport_shuttle,
+      // color: Colors.lightBlue,
+      // size: 50.0
+      //)
+      // child: Image.asset('assets/galaxy2.jpg'),
+      // child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/LH_95.jpg/330px-LH_95.jpg'),
+      //image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/LH_95.jpg/330px-LH_95.jpg'),
+      // image: AssetImage('assets/galaxy.jpg'),
+      //   )
+      ,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('click'),
